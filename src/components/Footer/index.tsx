4078537560f675c1e18c418bdc562b221f7e1d38
Serhii <vasylyk29@gmail.com>
2021-05-23 20:20:22 +0300

    added footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,15 +2,5 @@ import React from "react";
 import { FooterWrap } from "./styled-components";
 
 export default function Footer(): JSX.Element {
-  return (
-    <FooterWrap>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-      </a>
-    </FooterWrap>
-  );
+  return <FooterWrap>Copyright © 2021</FooterWrap>;
 }
